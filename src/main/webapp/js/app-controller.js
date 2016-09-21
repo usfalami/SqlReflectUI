4728fcef3413406578a5a74f5edca93d271bf940
YAH <youssef-externe.alami@erdf.fr>
2016-09-21 15:24:24 +0200

    evol

--- src/main/webapp/js/app-controller.js
+++ src/main/webapp/js/app-controller.js
@@ -4,7 +4,9 @@
 
 angular.module('app')
 
-.controller('consult', function($scope, $http){
+.controller('consult', function($scope, $http, $routeParams){
+	
+	console.log($routeParams);
 	
 	$scope.submit = function(uri){
 		$scope.reset();
--- src/main/webapp/js/app-route.js
+++ src/main/webapp/js/app-route.js
@@ -5,46 +5,18 @@
 angular.module('app')
 
 .config(['$routeProvider', function($routeProvider) {
-	$routeProvider.
-
-	when('/home', {
-		templateUrl: 'html/home.html', controller: 'consult'
-	}).
 	
-	when('/databases', {
-		templateUrl: 'html/databases.html', controller: 'consult'
-	}).
+	$routeProvider
 
-	when('/tables', {
-		templateUrl: 'html/tables.html', controller: 'consult'
-	}).
-	
-	when('/views', {
-		templateUrl: 'html/views.html', controller: 'consult'
-	}).
-
-	when('/columns', {
-		templateUrl: 'html/columns.html', controller: 'consult'
-	}).
-
-	when('/procedures', {
-		templateUrl: 'html/procedures.html', controller: 'consult'
-	}).
-
-	when('/arguments', {
-		templateUrl: 'html/arguments.html', controller: 'consult'
-	}).
-	
-	when('/rows', {
-		templateUrl: 'html/rows.html', controller: 'consult'
-	}).
-
-	when('/headers', {
-		templateUrl: 'html/headers.html', controller: 'consult'
-	}).
-	
-	otherwise({
-		redirectTo: '/home'
-	});
+	.when('/home', 		{templateUrl: 'html/home.html', controller: 'consult'})
+	.when('/databases', {templateUrl: 'html/databases.html', controller: 'consult'})
+	.when('/tables', 	{templateUrl: 'html/tables.html', controller: 'consult'})
+	.when('/views', 	{templateUrl: 'html/views.html', controller: 'consult'})
+	.when('/columns', 	{templateUrl: 'html/columns.html', controller: 'consult'})
+	.when('/procedures',{templateUrl: 'html/procedures.html', controller: 'consult'})
+	.when('/arguments', {templateUrl: 'html/arguments.html', controller: 'consult'})
+	.when('/rows', 		{templateUrl: 'html/rows.html', controller: 'consult'})
+	.when('/headers', 	{templateUrl: 'html/headers.html', controller: 'consult'})
+	.otherwise({redirectTo: '/home'});
 
 }]);
